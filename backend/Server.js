9b17b108ba1b9a6a72568872f56a58165648db47
Sachith Wijesiriwardana <sachith55cha1w@gmail.com>
2023-12-17 22:09:44 +0530

    adding mui

--- backend/Server.js
+++ backend/Server.js
@@ -1,5 +1,14 @@
-sql;
- db RTCPeerConnection
- sam;fmsdf
- msdfsdfmsdf
- msdfsdfmsdf
+const EventEmitter = require('events');
+
+class MyEmitter extends EventEmitter {}
+
+const myEmitter = new MyEmitter();
+
+// Adding 11 listeners to the 'close' event
+for (let i = 0; i < 11; i++) {
+  myEmitter.on('close', () => {
+    console.log('Close event triggered');
+  });
+}
+
+// You will see the warning here
